aacdb664a4b08fe4957575d66cdf71c225b02903
yhlyl <yh_android@163.com>
2017-08-15 00:54:34 +0800

    lambda表达式

--- app/src/main/java/org/yh/androidforjava8/MainActivity.java
+++ app/src/main/java/org/yh/androidforjava8/MainActivity.java
@@ -1,7 +1,9 @@
 package org.yh.androidforjava8;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity
 {
@@ -11,5 +13,24 @@ public class MainActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        new Thread(new Runnable()
+        {
+            public void run()
+            {
+                System.out.println("Run!");
+            }
+        }).start();
+
+        new Thread(() -> System.out.println("Run!")).start();
+        findViewById(R.id.text).setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+
+            }
+        });
+        findViewById(R.id.text).setOnClickListener(view -> Toast.makeText(this, "afadfasd", Toast
+                .LENGTH_SHORT).show());
     }
 }
