6a5afd22d4a4621a720d9eecf4ba411679def1d5
youhao <youh@fitcome.net>
2017-08-15 11:47:33 +0800

    Rxjava Rxandroid

--- app/src/main/java/org/yh/androidforjava8/MainActivity.java
+++ app/src/main/java/org/yh/androidforjava8/MainActivity.java
@@ -1,12 +1,24 @@
 package org.yh.androidforjava8;
 
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
+import java.util.Collections;
+
+import io.reactivex.Observable;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.observers.DisposableObserver;
+import io.reactivex.schedulers.Schedulers;
+
 public class MainActivity extends AppCompatActivity
 {
+    private static final String TAG = "MainActivity";
+    private final CompositeDisposable disposables = new CompositeDisposable();
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -30,7 +42,59 @@ public class MainActivity extends AppCompatActivity
 
             }
         });
-        findViewById(R.id.text).setOnClickListener(view -> Toast.makeText(this, "afadfasd", Toast
-                .LENGTH_SHORT).show());
+        findViewById(R.id.text).setOnClickListener(view ->
+        {
+            Toast.makeText(this, "afadfasd", Toast
+                    .LENGTH_SHORT).show();
+            onRunSchedulerExampleButtonClicked();
+        });
     }
+
+    @Override
+    protected void onDestroy()
+    {
+        super.onDestroy();
+        Collections.<String>emptyList();
+        disposables.clear();
+    }
+
+    void onRunSchedulerExampleButtonClicked()
+    {
+        disposables.add(sampleObservable()
+                // Run on a background thread
+                .subscribeOn(Schedulers.io())
+                // Be notified on the main thread
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeWith(new DisposableObserver<String>()
+                {
+                    @Override
+                    public void onComplete()
+                    {
+                        Log.e(TAG, "onComplete()");
+                    }
+
+                    @Override
+                    public void onError(Throwable e)
+                    {
+                        Log.e(TAG, "onError()", e);
+                    }
+
+                    @Override
+                    public void onNext(String string)
+                    {
+                        Log.e(TAG, "onNext(" + string + ")");
+                    }
+                }));
+    }
+
+    static Observable<String> sampleObservable()
+    {
+        return Observable.defer(() ->
+        {
+            // Do some long running operation
+            SystemClock.sleep(5000);
+            return Observable.just("one", "two", "three", "four", "five");
+        });
+    }
+
 }
